94271ffa01c02e4cc099923acfec75fcb1c1cc54
Eric Wood <eric@ericwood.org>
2026-01-17 20:38:32 -0700

    blog index page layout

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,30 @@
+const longFormatter = new Intl.DateTimeFormat(undefined, {
+  month: "short",
+  day: "numeric",
+  year: "numeric"
+});
+
+const shortFormatter = new Intl.DateTimeFormat(undefined, {
+  month: "2-digit",
+  day: "2-digit",
+  year: "2-digit",
+});
+
 const initTimestamps = () => {
   document.querySelectorAll('time').forEach((el) => {
+    let formatter = longFormatter
+    const isShort = el.dataset.short === "true";
+    if (isShort) {
+      formatter = shortFormatter;
+    }
+
     const timeStr = el.attributes.datetime;
     if (!timeStr || !timeStr.value) {
       return;
     }
 
     const date = new Date(timeStr.value);
-    const formatted = new Intl.DateTimeFormat(undefined, {
-      month: "short",
-      day: "numeric",
-      year: "numeric"
-    }).format(date);
+    const formatted = formatter.format(date);
 
     el.innerText = formatted;
   })
